fe70efdc62a4a5021b10b234e1453fb814a5d1f5
gustavo-munhoz <gustavo.munhoz015@gmail.com>
2023-03-06 22:00:25 -0300

    Initial Commit

---
+++ hellome/hellomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  hellomeApp.swift
+//  hellome
+//
+//  Created by Gustavo Munhoz Correa on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct hellomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
